5bb6f85ab140d3a0a03df391b2fde3fb1e6c49b9
Juan Morales <juancarlosmoralestellez@gmail.com>
2025-09-09 14:55:31 -0600

    Feat: implement Repository and Unit of Work design patterns

---
+++ src/Booking.Core/Interface/IRepository.cs
@@ -0,0 +1,12 @@
+namespace Booking.Core.Interface
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        //CRUD
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity?> GetById(int id);
+        Task Add(TEntity tentity);
+        void Update(TEntity tentity);
+        void Delete(TEntity tentity);
+    }
+}
