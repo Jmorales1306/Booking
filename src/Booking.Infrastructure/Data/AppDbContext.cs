320c032daf1e1a7907d0d6af55614f535ac42be3
Jmorales1306 <juancarlosmoralestellez@gmail.com>
2025-09-10 22:11:35 -0600

    Remove unnecessary using directive for Microsoft.EntityFrameworkCore in AppDbContext

--- src/Booking.Infrastructure/Data/AppDbContext.cs
+++ src/Booking.Infrastructure/Data/AppDbContext.cs
@@ -1,6 +1,4 @@
 using Booking.Core.Entities;
-using Microsoft.EntityFrameworkCore;
-
 namespace Booking.Infrastructure.Data
 {
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
