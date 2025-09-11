68c57c9fa6eac42d7959d2f3128dec46aa594950
Jmorales1306 <juancarlosmoralestellez@gmail.com>
2025-09-10 22:36:32 -0600

    Add global using directives for Booking.Api

---
+++ src/Booking.Api/GlobalUsing.cs
@@ -0,0 +1,7 @@
+global using Booking.Infrastructure.Data;
+global using Microsoft.EntityFrameworkCore;
+global using Booking.UseCases.Interfaces;
+global using Microsoft.AspNetCore.Mvc;
+global using Booking.Core.Interface;
+global using Booking.Infrastructure.Repositories.Implementations;
+global using Booking.UseCases.Services;
