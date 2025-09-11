bee6acf0ec0f23fdb4faf2647822610777ea3f42
Jmorales1306 <juancarlosmoralestellez@gmail.com>
2025-09-10 22:10:20 -0600

    Add global using directives for infrastructure and core entities

---
+++ src/Booking.Infrastructure/GlobalUsing.cs
@@ -0,0 +1,7 @@
+global using Booking.Infrastructure.Data;
+global using Microsoft.EntityFrameworkCore;
+global using Booking.Core.Interface;
+
+global using Reservation = Booking.Core.Entities.Booking;
+
+
