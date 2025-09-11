85290b225f9acbc7f4c7bc2df54e80437ff9efe5
Jmorales1306 <juancarlosmoralestellez@gmail.com>
2025-09-10 19:43:22 -0600

    Refactor global using directives in Booking.UseCases for clarity and conflict resolution

--- src/Booking.UseCases/GlobalUsing.cs
+++ src/Booking.UseCases/GlobalUsing.cs
@@ -1,3 +1,6 @@
 global using System.ComponentModel.DataAnnotations;
 global using Booking.Core.Interface;
 global using Booking.UseCases.Interfaces;
+
+// Alias to avoid conflicts
+global using Reservation = Booking.Core.Entities.Booking;
