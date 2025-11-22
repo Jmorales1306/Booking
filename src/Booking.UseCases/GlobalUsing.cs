2a24ff01c3a9c142102c95dcd97b807439184b6c
Juan Morales <juancarlosmoralestellez@gmail.com>
2025-11-21 23:28:08 -0600

    Add Booking.Core.Exceptions to global usings for improved exception handling

--- src/Booking.UseCases/GlobalUsing.cs
+++ src/Booking.UseCases/GlobalUsing.cs
@@ -1,6 +1,8 @@
 global using System.ComponentModel.DataAnnotations;
 global using Booking.Core.Interface;
 global using Booking.UseCases.Interfaces;
+global using Booking.Core.Exceptions;
+
 
 // Alias to avoid conflicts
 global using Reservation = Booking.Core.Entities.Booking;
